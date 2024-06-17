edd648043c8febc2ed41bebbb0625887a57a8b73
Jakob <jt@greyd.de>
2024-06-17 09:53:50 +0200

    first screen kind of there

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './assets/css/primitive.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [url, setUrl] = useState('');
+	const [result, setResult] = useState(null);
+	const [error, setError] = useState(null);
+
+	const checkThemeJson = async () => {
+		// try {
+		// 	const response = await axios.post('/api/check-theme-json', { url });
+		// 	setResult(response.data);
+		// 	setError(null);
+		// } catch (err) {
+		// 	setError('Error fetching theme.json or URL is not a WordPress site with a theme.json file.');
+		// 	setResult(null);
+		// }
+	};
+
+	return (
+	<div className="small-container">
+		<h1>Theme JSON Checker</h1>
+		<div
+		style={{
+			display: "flex",
+			alignItems: "flex-end",
+			gap: "1em"
+		}}
+		>
+			<div
+			style={{
+				flexGrow: 1
+			}}>
+				<label>
+					Enter website URL
+				</label>
+				<input
+					type="text"
+					value={url}
+					onChange={(e) => setUrl(e.target.value)}
+					placeholder="https://www.greyd.io"
+				/>
+			</div>
+			<div>
+				<button onClick={checkThemeJson}>Check Theme</button>
+			</div>
+		</div>
+		{error && <p className="error">{error}</p>}
+		{result && (
+			<div className="result">
+			<h2>Features:</h2>
+			<ul>
+				{Object.keys(result).map((feature) => (
+				<li key={feature}>
+					{feature}: {result[feature] ? 'Yes' : 'No'}
+					<p>{result[feature].description}</p>
+				</li>
+				))}
+			</ul>
+			</div>
+		)}
+	</div>
+	);
 }
 
 export default App;
